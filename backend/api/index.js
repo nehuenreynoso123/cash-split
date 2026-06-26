eacf2e269f83d0e450e7ddca30c2c31ae13936a2
Nehuen Reynoso <nehuenreynoso123@gmail.com>
2026-06-26 20:19:43 -0300

    debug: handler minimo para test

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,17 +1,3 @@
-import express from 'express'
-import '../store/database.js'
-import routes from '../api_cash_split/routes.js'
-import morgan from 'morgan'
-import cors from 'cors'
-import { errors } from '../api_cash_split/network/error.js'
-
-const app = express()
-
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-routes(app)
-app.use(errors)
-
-export default app
+export default (req, res) => {
+  res.status(200).json({ status: 'OK' })
+}
