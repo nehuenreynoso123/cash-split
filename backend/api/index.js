ab6d3820aed9a80ae23dab07779dce6b6b33e823
Nehuen Reynoso <nehuenreynoso123@gmail.com>
2026-06-26 20:21:13 -0300

    debug: express simple sin middleware

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,9 @@
-export default (req, res) => {
-  res.status(200).json({ status: 'OK' })
-}
+import express from 'express'
+
+const app = express()
+
+app.get('/api/status', (req, res) => {
+  res.json({ error: false, status: 200, body: { status: 'OK' } })
+})
+
+export default app
